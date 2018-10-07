fd449d9a3fe999e9c1fb3c0417d7394178e1c83a
王宏博实验室pc <920609306@qq.com>
2018-10-07 22:09:57 +0800

    m修改主界面导航栏按钮样式

---
+++ SCOS/app/src/main/java/es/source/code/activity/LoginOrRegister.java
@@ -0,0 +1,11 @@
+package es.source.code.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2018/10/7.
+ */
+
+public class LoginOrRegister extends AppCompatActivity {
+
+}
---
+++ SCOS/app/src/main/java/es/source/code/activity/MainScreen.java
@@ -0,0 +1,10 @@
+package es.source.code.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by admin on 2018/10/7.
+ */
+
+public class MainScreen extends AppCompatActivity {
+}
