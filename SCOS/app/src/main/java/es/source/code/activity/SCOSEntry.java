4c200c6519cfd4861f38280172cb072b6613e9ab
王宏博实验室pc <920609306@qq.com>
2018-10-09 22:50:09 +0800

    完成对滑动手势的检测

--- SCOS/app/src/main/java/es/source/code/activity/SCOSEntry.java
+++ SCOS/app/src/main/java/es/source/code/activity/SCOSEntry.java
@@ -2,6 +2,7 @@ package es.source.code.activity;
 
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,9 +14,10 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
-public class SCOSEntry extends AppCompatActivity implements View.OnTouchListener {
 
-    private GestureDetector gestureDetector;
+public class SCOSEntry extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,44 +28,66 @@ public class SCOSEntry extends AppCompatActivity implements View.OnTouchListener
         relativeLayout.setOnTouchListener(this);
         relativeLayout.setLongClickable(true);
         */
-        gestureDetector = new GestureDetector(SCOSEntry.this, onGestureListener);
-        Button btn = (Button) findViewById(R.id.btn);
-        btn.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                //Toast.makeText(SCOSEntry.this, "You click the button", Toast.LENGTH_LONG).show();
-                Log.d("ESCOSnrty", "onClick:btn ");
-                //隐式调用
-                Intent intent = new Intent("scos.intent.action.SCOSMAIN");
-                intent.addCategory("scos.intent.category.SCOSLAUNCHER");
-                intent.putExtra("info","FromEntry");
-                startActivity(intent);
-                Log.d("ESCOSnrty", "onClick: intent");
-            }
-
-        });
+        gestureDetector = new GestureDetector(SCOSEntry.this, this);
+//        Button btn = (Button) findViewById(R.id.btn);
+//        btn.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Toast.makeText(SCOSEntry.this, "You click the button", Toast.LENGTH_LONG).show();
+//                Log.d("ESCOSnrty", "onClick:btn ");
+//                //隐式调用
+//                Intent intent = new Intent("scos.intent.action.SCOSMAIN");
+//                intent.addCategory("scos.intent.category.SCOSLAUNCHER");
+//                intent.putExtra("info","FromEntry");
+//                startActivity(intent);
+//                Log.d("ESCOSnrty", "onClick: intent");
+//            }
+//
+//        });
     }
 
-    private GestureDetector.OnGestureListener onGestureListener = new GestureDetector.SimpleOnGestureListener() {
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            float x = e2.getX() - e1.getX();
-            if (x < 0) {
-                Intent intent = new Intent();
-                intent.setClass(SCOSEntry.this, MainScreen.class);
-                Bundle bundle = new Bundle();
-                bundle.putString("info", "FromEntry");
-                intent.putExtras(bundle);
-                startActivity(intent);
-            }
-            return true;
-        }
-    };
-
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
+    public boolean onTouchEvent(MotionEvent event) {
         return gestureDetector.onTouchEvent(event);
     }
 
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        Log.d("Touch", "onDown:检测 ");
+        return true;
+    }
 
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float v, float v1) {
+        float x = e1.getX() - e2.getX();
+        if (x > 50) {
+            Log.d("Infosssss", "onFling: 滑动检测");
+            Intent intent = new Intent("scos.intent.action.SCOSMAIN");
+            intent.addCategory("scos.intent.category.SCOSLAUNCHER");
+            intent.putExtra("info","FromEntry");
+            SCOSEntry.this.startActivity(intent);
+            return  true;
+        }
+        return false;
+    }
 }
